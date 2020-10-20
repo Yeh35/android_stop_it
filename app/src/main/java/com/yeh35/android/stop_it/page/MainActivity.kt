4282fde995e986d764d172eb16f7b089435f9f44
jaws0227 <jaws0910@gmail.com>
2020-10-20 18:12:58 +0900

    Main Activity 구성

--- app/src/main/java/com/yeh35/android/stop_it/page/MainActivity.kt
+++ app/src/main/java/com/yeh35/android/stop_it/page/MainActivity.kt
@@ -2,16 +2,92 @@ package com.yeh35.android.stop_it.page
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomappbar.BottomAppBar
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 import com.yeh35.android.stop_it.R
+import com.yeh35.android.stop_it.page.home.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import org.joda.time.DateTime
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    lateinit var bottomAppBar: BottomAppBar
+    private var viewStatus: ViewStatus? = null
+    private var onHomeBackPressedTime: DateTime = DateTime.now()
+    private val homeFragment = HomeFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_home.setOnClickListener(this)
+        btn_user.setOnClickListener(this)
+
+        replaceFragment(ViewStatus.HOME)
     }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            btn_home -> {
+                replaceFragment(ViewStatus.HOME)
+            }
+            btn_user -> {
+                replaceFragment(ViewStatus.USER)
+            }
+            else -> {
+                error("정의 하지 않은 View 입니다.")
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (viewStatus == ViewStatus.HOME) {
+            if (onHomeBackPressedTime.plusMillis(200).isAfterNow) {
+                onHomeBackPressedTime = DateTime.now()
+                Snackbar.make(fragment_container, "한번더 누르면 앱이 종료됩니다.", Snackbar.LENGTH_SHORT).show()
+            } else {
+                super.onBackPressed()
+            }
+
+
+        } else {
+            replaceFragment(ViewStatus.HOME)
+        }
+    }
+
+    private fun replaceFragment(viewStatus: ViewStatus) {
+        if (this.viewStatus == viewStatus) {
+            return
+        }
+
+        this.viewStatus = viewStatus
+
+        when (viewStatus) {
+            ViewStatus.HOME -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragment_container, homeFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+
+            ViewStatus.USER -> {
+//                val transaction = supportFragmentManager.beginTransaction()
+////                transaction.replace(R.id.fragment_container, startingFragment)
+//                transaction.addToBackStack(null)
+//                transaction.commit()
+            }
+
+            else -> {
+                error("정의되지 않은 상태입니다.")
+            }
+        }
+
+        btn_home.setImageResource(if (viewStatus == ViewStatus.HOME) R.drawable.icons_home_full else R.drawable.button_home_states)
+        btn_user.setImageResource(if (viewStatus == ViewStatus.USER) R.drawable.icons_user_full else R.drawable.button_user_states)
+    }
+
+    private enum class ViewStatus {
+        HOME,
+        USER
+    }
+
 }
