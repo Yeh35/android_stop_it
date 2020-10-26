2fdd3f58751c8c3fe677dc1bfe541cc5e2dcb4b4
jaws0227 <jaws0910@gmail.com>
2020-10-26 15:30:19 +0900

    불멸의 서비스 완료!

---
+++ app/src/main/java/com/yeh35/android/stop_it/broadcast/RestartReceiver.kt
@@ -0,0 +1,46 @@
+package com.yeh35.android.stop_it.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import com.yeh35.android.stop_it.service.OnLockService
+
+class RestartReceiver : BroadcastReceiver() {
+
+    companion object {
+        const val ACTION_RESTART_ON_LOCK_SERVICE = "com.yeh35.android.stop_it.action.restart.ON_LOCK_SERVICE"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        Log.i("RestartService" , "RestartService called : " + intent.action)
+
+        when (intent.action) {
+            ACTION_RESTART_ON_LOCK_SERVICE,
+            Intent.ACTION_BOOT_COMPLETED -> {
+                Log.i("RestartService" , "ON_LOCK_SERVICE")
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val serviceIntent = Intent(context, OnLockService::class.java)
+                    context.startForegroundService(serviceIntent)
+                } else {
+                    val serviceIntent = Intent(context, OnLockService::class.java)
+                    context.startService(serviceIntent)
+                }
+            }
+
+            else -> {
+                error("정의하지 않은 ACTION 입니다.")
+            }
+        }
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val serviceIntent = Intent(context, OnLockService::class.java)
+//            context.startForegroundService(serviceIntent)
+//        } else {
+//            val serviceIntent = Intent(context, OnLockService::class.java)
+//            context.startService(serviceIntent)
+//        }
+    }
+}
--- app/src/main/java/com/yeh35/android/stop_it/service/OnLockService.kt
+++ app/src/main/java/com/yeh35/android/stop_it/service/OnLockService.kt
@@ -8,13 +8,13 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.IBinder
+import android.os.SystemClock
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import com.yeh35.android.stop_it.broadcast.AlarmReceiver
+import com.yeh35.android.stop_it.broadcast.RestartReceiver
 import com.yeh35.android.stop_it.broadcast.ScreenReceiver
 import com.yeh35.android.stop_it.page.MainActivity
-import java.util.*
 
 
 class OnLockService : Service() {
@@ -25,73 +25,71 @@ class OnLockService : Service() {
 
     private var mReceiver: BroadcastReceiver? = null
 
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
     override fun onCreate() {
         Log.d("OnLockService", "onCreate")
         super.onCreate()
+
         val filter = IntentFilter(Intent.ACTION_SCREEN_ON)
         filter.addAction(Intent.ACTION_SCREEN_OFF)
         mReceiver = ScreenReceiver()
         registerReceiver(mReceiver, filter)
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("OnLockService", "onStartCommand")
 
-        this.initializeNotification()
+        //startForeground(1, Notification())
 
-        if (intent == null) {
-            val filter = IntentFilter(Intent.ACTION_SCREEN_ON)
-            filter.addAction(Intent.ACTION_SCREEN_OFF)
-            mReceiver = ScreenReceiver()
-            registerReceiver(mReceiver, filter)
-
-        } else {
-            serviceIntent = intent
-        }
-
-        Log.d("OnLockService", "serviceIntent : " + serviceIntent.toString())
-        return START_STICKY
-    }
-
-    override fun onBind(intent: Intent?): IBinder? {
-        Log.d("OnLockService", "onBind")
-        return null
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun initializeNotification() {
-        val builder = NotificationCompat.Builder(this, "1")
-        builder.setSmallIcon(R.mipmap.sym_def_app_icon)
+        /**
+         * startForeground 를 사용하면 notification 을 보여주어야 하는데 없애기 위한 코드
+         */
         val style = NotificationCompat.BigTextStyle()
         style.bigText("설정을 보려면 누르세요.")
         style.setBigContentTitle(null)
         style.setSummaryText("서비스 동작중")
-        builder.setContentText(null)
-        builder.setContentTitle(null)
-        builder.setOngoing(true)
-        builder.setStyle(style)
-        builder.setWhen(0)
-        builder.setShowWhen(false)
 
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
-        builder.setContentIntent(pendingIntent)
 
-        val manager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification = NotificationCompat.Builder(this, "1")
+            .setSmallIcon(R.mipmap.sym_def_app_icon)
+            .setContentText(null)
+            .setContentTitle(null)
+            .setOngoing(true)
+            .setStyle(style)
+            .setWhen(0)
+            .setShowWhen(false)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            manager.createNotificationChannel(
+            notificationManager.createNotificationChannel(
                 NotificationChannel(
                     "1",
-                    "undead_service",
+                    "on_lock_service",
                     NotificationManager.IMPORTANCE_NONE
                 )
             )
         }
 
-        val notification = builder.build()
-        startForeground(1, notification)
+        notificationManager.notify(startId, notification)
+        notificationManager.cancel(startId)
+
+        if (mReceiver == null) {
+            val filter = IntentFilter(Intent.ACTION_SCREEN_ON)
+            filter.addAction(Intent.ACTION_SCREEN_OFF)
+            mReceiver = ScreenReceiver()
+            registerReceiver(mReceiver, filter)
+        }
+
+//        return START_STICKY
+        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
@@ -102,17 +100,18 @@ class OnLockService : Service() {
             mReceiver = null
         }
 
-
         Log.d("OnLockService", "onDestroy")
 
-        val calendar: Calendar = Calendar.getInstance()
-        calendar.timeInMillis = System.currentTimeMillis()
-        calendar.add(Calendar.SECOND, 3)
+        registerRestartAlarm()
 
-        val intent = Intent(this, AlarmReceiver::class.java)
-        val sender = PendingIntent.getBroadcast(this, 0, intent, 0)
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager[AlarmManager.RTC_WAKEUP, calendar.timeInMillis] = sender
+//        val calendar: Calendar = Calendar.getInstance()
+//        calendar.timeInMillis = System.currentTimeMillis()
+//        calendar.add(Calendar.SECOND, 3)
+//
+//        val intent = Intent(this, RestartReceiver::class.java)
+//        val sender = PendingIntent.getBroadcast(this, 0, intent, 0)
+//        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//        alarmManager[AlarmManager.RTC_WAKEUP, calendar.timeInMillis] = sender
     }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
@@ -125,13 +124,45 @@ class OnLockService : Service() {
 
         Log.d("OnLockService", "onTaskRemoved")
 
-        val calendar: Calendar = Calendar.getInstance()
-        calendar.timeInMillis = System.currentTimeMillis()
-        calendar.add(Calendar.SECOND, 3)
+        registerRestartAlarm()
 
-        val intent = Intent(this, AlarmReceiver::class.java)
-        val sender = PendingIntent.getBroadcast(this, 0, intent, 0)
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager[AlarmManager.RTC_WAKEUP, calendar.timeInMillis] = sender
+//        val calendar: Calendar = Calendar.getInstance()
+//        calendar.timeInMillis = System.currentTimeMillis()
+//        calendar.add(Calendar.SECOND, 3)
+//
+//        val intent = Intent(this, RestartReceiver::class.java)
+//        val sender = PendingIntent.getBroadcast(this, 0, intent, 0)
+//        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//        alarmManager[AlarmManager.RTC_WAKEUP, calendar.timeInMillis] = sender
     }
+
+    /**
+     * 알람 매니져에 서비스 등록
+     */
+    private fun registerRestartAlarm() {
+        Log.i("OnLockService", "registerRestartAlarm")
+
+        val intent = Intent(this, RestartReceiver::class.java)
+        intent.action = RestartReceiver.ACTION_RESTART_ON_LOCK_SERVICE
+        val sender = PendingIntent.getBroadcast(this, 0, intent, 0)
+
+        val firstTime = SystemClock.elapsedRealtime() + 1*1000
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime, 100, sender)
+    }
+
+    /**
+     * 알람 매니져에 서비스 해제
+     */
+    private fun unregisterRestartAlarm() {
+        Log.i("OnLockService", "unregisterRestartAlarm")
+
+        val intent = Intent(this, RestartReceiver::class.java)
+        intent.action = RestartReceiver.ACTION_RESTART_ON_LOCK_SERVICE
+        val sender = PendingIntent.getBroadcast(this, 0, intent, 0)
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.cancel(sender)
+    }
+
 }
